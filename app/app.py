3f11405553e41a623b5554d960431c6cfe6b3b61
shiqilouyang <39560143+shiqilouyang@users.noreply.github.com>
2021-02-04 09:41:17 +0800

    Update app.py
    [fix]  update dataCent path to with more codes

--- app/app.py
+++ app/app.py
@@ -1,42 +1,35 @@
-from queue import Queue
-from flask import  abort
-from info import app
-from conf.Setting import   utils_init_path
-from geventwebsocket.handler import WebSocketHandler
-from geventwebsocket.server import WSGIServer
+'''
+allure 使用文档
+https://cloud.tencent.com/developer/article/1516907
+
+运行命令:
+    pytest
+    copy environment.xml allure-results
+    allure generate allure-results -o allure-reports/ --clean
+    allure open -h 127.0.0.1 -p 8883 ./allure-reports
 
 
+pytest.ini 配置了 pytest 启动参数 --clean-alluredir 表示每次启动都会清除历史运行记录
 
-at = abort
-path = utils_init_path
-body = {}
-client_list = []
-q = Queue()
+environment.xml  保存了 allure 配置参数, 但是需要在 allure-results 目录之中才能生效,所以linux/win每次启动都要 cp/copy
 
-# @app.route('/websocket')
-# def websocket():
-#      client_socket = request.environ.get('wsgi.websocket')
-#      client_list.append(client_socket)
-#      userId_ = request.args.get("userId")
-#      from TestPoolModel.model import Operationtable
-#      # print(len(client_list), client_list)
-#      while 1:
-#          msg_from_cli = client_socket.receive()
-#          print(msg_from_cli)
-#          #收到任何一个客户端的信息都进行全部转发（注意如果某个客户端连接断开，在遍历发送时连接不存在会报错，需要异常处理）
-#          for client in client_list:
-#              try:
-#                  userMessage = Operationtable.query.filter(Operationtable.user_id == msg_from_cli).first()
-#                  client.send(userMessage.message)
-#                  print(userMessage.message)
-#              except Exception as e:
-#                  continue
+'''
 
-
-# if __name__ =="__main__":
-#     app.run(host="0.0.0.0", threaded=True, debug=True, port=8080)
-
+import os
+import subprocess
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('0.0.0.0', 8080), application=app, handler_class=WebSocketHandler)
-    http_server.serve_forever()
+    ''' 测试实例启动脚本  '''
+    dataCents_path = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+    environment_path = os.path.join(dataCents_path, "environment.xml")
+    allure_results_path = os.path.join(dataCents_path, "allure-results")
+    allure_reports_path = os.path.join(dataCents_path, "allure-reports")
+    cmds = [
+        "cd {} & pytest".format(dataCents_path),
+        'copy {} {}'.format(environment_path,allure_results_path),
+        'allure generate {} -o {} --clean'.format(allure_results_path,allure_reports_path),
+        'allure open -h 127.0.0.1 -p 8883 {}'.format(allure_reports_path)
+    ]
+    for i in cmds:
+        child = subprocess.Popen(i,shell=True)
+        child.wait()
